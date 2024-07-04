9468c9b7520abe5e87c30801c9e88adaab9d3d03
Florcita 🌸 <kaniflorencia@gmail.com>
2024-07-04 01:01:19 -0400

    Actualizar owner-autobio.js

--- plugins/owner-autobio.js
+++ plugins/owner-autobio.js
@@ -8,7 +8,7 @@ let handler = m => m
  _muptime = await new Promise(resolve => { process.once('message', resolve)  
  setTimeout(resolve, 2000) }) * 1000} 
  let uptime = clockString(_uptime) 
- let bio = `🚀 𝙒𝙄𝙀𝙍 𝘽𝙊𝙏 𝙋𝙍𝙊 ⣿  👹 𝙈𝙖𝙭𝙞𝙢𝙪𝙣 𝙋𝙤𝙬𝙚𝙧 𝙊𝙉𝙇𝙄𝙉𝙀 𝘿𝙪𝙧𝙖𝙣𝙩𝙚 ${uptime}` 
+ let bio = `🌷 𝗬𝘂𝗹𝗶 𝗕𝗼𝘁 ┆ 🍁 𝗔𝗸𝗶𝗿𝗮𝗮𝗕𝗼𝘁 𝙊𝙉𝙇𝙄𝙉𝙀 𝘿𝙪𝙧𝙖𝙣𝙩𝙚 ${uptime}` 
  await this.updateProfileStatus(bio).catch(_ => _) 
  setting.status = new Date() * 1 
  }  
