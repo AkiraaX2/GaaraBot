342c36078667f30e0b5f9d32cfc1084aacf3bc8d
Florcita 🌸 <kaniflorencia@gmail.com>
2024-07-23 01:25:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['56964886274', '𝑶𝒘𝒏𝒆𝒓 🌹', true],
-  ['56945843173', '𝑪𝒓𝒆𝒂𝒅𝒐𝒓𝒂 🌷', true],
+  ['56945843173', '𝑪𝒓𝒆𝒂𝒅𝒐𝒓/𝒂 🍯', true],
   ['59168683798', '𝑪𝒐𝒍𝒂𝒃 🍁', true],
 ] //Numeros de owner 
 
@@ -39,9 +38,9 @@ global.bgp = 'https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6'
 global.bgp2 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O'
 global.bgp3 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O' //--GP NSFW
 
-global.wait = '🐒𝐂𝐚𝐫𝐠𝐚𝐧𝐝𝐨....'
+global.wait = '🔥𝐂𝐚𝐫𝐠𝐚𝐧𝐝𝐨....'
 global.rwait = '⌛'
-global.dmoji = '🌷'
+global.dmoji = '🍯'
 global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🔥' 
