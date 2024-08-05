d14f319e76b200479eb73e7d69b52b5c9ebb1a50
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-04 21:44:37 -0300

    Actualizar rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -3,7 +3,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 m.reply(`
-▢ *${mssg.numSn}* : ${sn}
+ִ࣪꩜» *${mssg.numSn}* : ${sn}
 `.trim())
 }
 handler.help = ['mysn']
