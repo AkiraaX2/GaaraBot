35ed1893b6581d3cc6ab8cc96fa8ccb615a93e90
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-04 21:46:37 -0300

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,7 +1,7 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ ${mssg.snVerify}\n\n${usedPrefix}nserie`
+  if (!args[0]) throw `ִ࣪꩜» ${mssg.snVerify}\n\n${usedPrefix}nserie`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw `⚠️ *${mssg.snError}*`
