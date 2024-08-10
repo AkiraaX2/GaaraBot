8a12fb8403c4def26898ede47937bf40a7f671d9
Florcita 🌸 <kaniflorencia@gmail.com>
2024-08-10 01:19:01 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -3,7 +3,7 @@ m.react('🫅')
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:𝑮𝒂𝒂𝒓𝒂 💥\nFN:𝑮𝒂𝒂𝒓𝒂 💥\nORG:𝑮𝒂𝒂𝒓𝒂 💥\nTITLE:\nitem1.TEL;waid=56945843173:56945843173\nitem1.X-ABLabel:𝑮𝒂𝒂𝒓𝒂 💥\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝑮𝒂𝒂𝒓𝒂 💥\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:𝑮𝒂𝒂𝒓𝒂 💥\nFN:𝑮𝒂𝒂𝒓𝒂 💥\nORG:𝑮𝒂𝒂𝒓𝒂 💥\nTITLE:\nitem1.TEL;waid=56986511915:56986511915\nitem1.X-ABLabel:𝑮𝒂𝒂𝒓𝒂 💥\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝑮𝒂𝒂𝒓𝒂 💥\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: '𝑮𝒂𝒂𝒓𝒂 💥', contacts: [{ vcard }] }}, {quoted: fkontak})
 }
 handler.help = ['owner']
