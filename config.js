7a1539262de37e98f1b454dade8aa7df3361ede2
Florcita 🌸 <kaniflorencia@gmail.com>
2024-08-31 13:56:53 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['56986511915', '𝑪𝒓𝒆𝒂𝒅𝒐𝒓/𝒂 🍯', true],
+  ['59160267791', '𝑪𝒓𝒆𝒂𝒅𝒐𝒓/𝒂 🍯', true],
   ['59168683798', '𝑪𝒐𝒍𝒂𝒃 🍁', true],
 ] //Numeros de owner 
 
