a7ee1cac21bd43af726f2b04ba47fa08d8cb6b7a
Florcita 🌸 <kaniflorencia@gmail.com>
2024-09-08 21:58:40 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✅ Iniciando...')
+console.log('🍯 Iniciando...🍯')
 
 import { join, dirname } from 'path'
 import { createRequire } from 'module';
@@ -24,19 +24,19 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Genesis Bot', {
+say('Gaara Bot', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`Genesis-Bot By: Maxzito`, {
+say(`GaaraBot By: Florcita`, {
   font: 'console',
   align: 'center',
   gradient: ['cyan', 'magenta']
 })
 
 app.listen(port, () => {
-  console.log(chalk.green(`🍒 Puerto ${port} esta abierto`));
+  console.log(chalk.green(`🍯 Puerto ${port} esta abierto 🍯`));
 });
 
 var isRunning = false
@@ -88,7 +88,7 @@ async function start(file) {
   console.log(chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`));
   const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
   console.log(chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`));
-  console.log(chalk.yellow(`📃 Script by FG98`));
+  console.log(chalk.yellow(`📃 Script by Mxz`));
 
   const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json');
     try {
